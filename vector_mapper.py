66dd244e9c5119febb65f6d1a5748d4bc5dac3c4
Muizudeen Kusimo <muizz@inspaya.com>
2018-05-09 18:40:22 -0700

    Clean up code

--- vector_mapper.py
+++ vector_mapper.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
+from os import environ
 from uuid import uuid4
 from tempfile import NamedTemporaryFile
-from subprocess import Popen, PIPE
+from subprocess import Popen, check_output, PIPE
 from collections import namedtuple
 
-FASTTEXT_EXE_PATH = '/path/to/fastText'
+FASTTEXT_EXE_PATH = environ.get('FASTTEXT_EXE', '/path/to/fastText')
 MODEL_BIN_PATH = 'model.bin'
 
-# we manually generate the ID since we use SQlite for this example
-SentenceRecord = namedtuple('VectorsTuple', 'id sentence_string word_vectors_list')
+SentenceRecord = namedtuple('SentenceRecord', 'id sentence_string word_vectors_list')
 sentence_to_vector_list = []
+
 
 def parse_input_file(input_file_name):
     input_file_contents = open(input_file_name, 'r').readlines()
@@ -20,16 +21,10 @@ def parse_input_file(input_file_name):
 
         temp_sentence_file = NamedTemporaryFile(delete=False)
         temp_sentence_file.file.write(str.encode(sentence))
-        print(temp_sentence_file.name)
-        exit()
-        fasttext_process = Popen([FASTTEXT_EXE_PATH, 'print-word-vectors', MODEL_BIN_PATH], stdin=open(temp_sentence_file.name, 'r'), stdout=PIPE)
+        output = check_output([FASTTEXT_EXE_PATH, 'print-word-vectors', MODEL_BIN_PATH],
+                              stdin=open(temp_sentence_file.name, 'r'))
 
-        output, error = fasttext_process.communicate()
-        fasttext_status = fasttext_process.wait()
-
-        print(fasttext_status)
         print(output)
-        print(error)
         exit()
 
         if output:
@@ -49,8 +44,7 @@ def retrieve_word_vectors_from_output(output):
     vectors_found = [possible_vector for possible_vector in output_vectors_list if isinstance(possible_vector, str)]
     return vectors_found
 
+
 if __name__ == '__main__':
     parse_input_file('simple_sentences.txt')
     print(sentence_to_vector_list)
-
-
